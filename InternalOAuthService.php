cf28d034725f187cb97ea8f1c8d28dea50110f53
Alex Salt <alex.salt@e96.ru>
2014-08-21 22:54:46 +0600

    initial commit

---
+++ InternalOAuthService.php
@@ -0,0 +1,32 @@
+<?php
+// vim: sw=4:ts=4:noet:sta:
+
+class InternalOAuthService extends EOAuth2Service {
+	protected $accountInfoUrl;
+
+	protected $uid = null;
+
+	protected function getAccessToken($code) {
+		$params = array(
+			'client_id' => $this->client_id,
+			'client_secret' => $this->client_secret,
+			'grant_type' => 'authorization_code',
+			'code' => $code,
+			'redirect_uri' => $this->getState('redirect_uri'),
+		);
+		return $this->makeRequest($this->getTokenUrl($code), [ 'data' => $params ]);
+	}
+
+	protected function saveAccessToken($token) {
+		$this->setState('auth_token', $token->access_token);
+		$this->setState('expires', time() + (isset($token->expires_in) ? $token->expires_in : 365 * 86400) - 60);
+		$this->access_token = $token->access_token;
+	}
+
+	protected function fetchAttributes() {
+		$info = $this->makeSignedRequest($this->accountInfoUrl);
+		$this->attributes['id'] = $info->id;
+		$this->attributes['login'] = $info->login;
+		$this->attributes['name'] = $info->name;
+	}
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# How to use
+* Положить в расширения
+* Использовать вместе с <https://github.com/Nodge/yii-eauth/>
