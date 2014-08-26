a6d66991f0f82e5d3a19d2063e16585edbb1bd58
Alex Salt <alex.salt@e96.ru>
2014-08-27 00:13:26 +0600

    internal oauth action

---
+++ InternalOAuthAction.php
@@ -0,0 +1,43 @@
+<?php
+// vim: sw=4:ts=4:noet:sta:
+
+/**
+ * Внутренняя аутентификация через OAauth
+ */
+class InternalOAuthAction extends CAction {
+
+	/**
+	 * @var string название компонента с пользователем
+	 */
+	public $userComponent = 'user';
+
+	/**
+	 * @var string название компонента с EAuth
+	 */
+	public $eauthComponent = 'eauth';
+
+	/**
+	 * @var string название сервиса для авторизации
+	 */
+	public $eauthService = 'e96_auth';
+
+	public function run() {
+		$eauth = Yii::app()->getComponent($this->eauthComponent)
+			->getIdentity($this->eauthService);
+		$user = Yii::app()->getComponent($this->userComponent);
+
+		if ($eauth->authenticate()) {
+			$identity = new EAuthUserIdentity($eauth);
+
+			if ($identity->authenticate()) {
+				$user->login($identity);
+				$this->controller->redirect($user->returnUrl);
+			} else {
+				$eauth->cancel();
+			}
+			throw new CException('Неведомая фигня');
+		} else {
+			throw new CException('Неведомая ошибка при авторизации');
+		}
+	}
+}
